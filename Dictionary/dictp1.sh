0495e884422a88cd85bf777ca6504daad40f8321
unknown <chaware.ajinkya59@gmail.com>
2021-01-06 15:55:22 +0530

    Basic dictionary Problems

---
+++ Dictionary/dictp1.sh
@@ -0,0 +1,18 @@
+
+#!/bin/bash -x
+
+declare -A die
+
+r2=$((1+RANDOM%6))
+r3=$((1+RANDOM%6))
+r4=$((1+RANDOM%6))
+r5=$((1+RANDOM%6))
+r6=$((1+RANDOM%6))
+
+while [ a!=15 ]
+do
+	die[i]=$((1+RANDOM%6))
+
+done
+
+echo "o/p is " ${die[@]}
